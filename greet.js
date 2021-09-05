3a3d02411d796b228328d11fab77a0ac033ae1cf
Mohau <mohaumodisana@gmail.com>
2021-09-05 17:14:31 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,10 @@
+// function greet(N_Name){
+//     return 'Hello, '+N_Name;
+//   }
+
+//   var msg = greet('Xola');
+//   console.log(msg);
+
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
